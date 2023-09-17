f5ca7f1dd24ed2483903702ba9a2f83ce43e4958
Hiptostee <joey.marra2007@gmail.com>
2023-09-17 03:03:25 -0400

    better margin

--- pages/about.js
+++ pages/about.js
@@ -90,7 +90,7 @@ export default function Home() {
               <div className="mx-auto  mt-16 max-w-2xl gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none  lg:items-start lg:gap-y-10">
                 <div className="lg:pr-4">
                   <div className="max-w-xl text-base leading-7 text-white lg:max-w-full">
-                    <div className=" lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                    <div className=" m-8 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
                       <div>
                         <h1 className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl text-center">
                           Chem Tools

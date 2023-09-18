1d86d30b79e1ed67ef0a34da9171344008f73dce
Hiptostee <joey.marra2007@gmail.com>
2023-09-17 22:48:37 -0400

    testimonials

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Head from "next/head";
 import Image from "next/image";
 import data from "../data.json";
@@ -18,6 +19,73 @@ import {
   NewspaperIcon,
   BeakerIcon,
 } from "@heroicons/react/24/outline";
+const featuredTestimonial = {
+  body: "Integer id nunc sit semper purus. Bibendum at lacus ut arcu blandit montes vitae auctor libero. Hac condimentum dignissim nibh vulputate ut nunc. Amet nibh orci mi venenatis blandit vel et proin. Non hendrerit in vel ac diam.",
+  author: {
+    name: "Brenna Goyette",
+    handle: "brennagoyette",
+    imageUrl:
+      "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=1024&h=1024&q=80",
+    logoUrl: "https://tailwindui.com/img/logos/savvycal-logo-gray-900.svg",
+  },
+};
+const testimonials = [
+  [
+    [
+      {
+        body: "Laborum quis quam. Dolorum et ut quod quia. Voluptas numquam delectus nihil. Aut enim doloremque et ipsam.",
+        author: {
+          name: "Leslie Alexander",
+          handle: "lesliealexander",
+          imageUrl:
+            "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+      },
+      // More testimonials...
+    ],
+    [
+      {
+        body: "Aut reprehenderit voluptatem eum asperiores beatae id. Iure molestiae ipsam ut officia rem nulla blanditiis.",
+        author: {
+          name: "Lindsay Walton",
+          handle: "lindsaywalton",
+          imageUrl:
+            "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+      },
+      // More testimonials...
+    ],
+  ],
+  [
+    [
+      {
+        body: "Voluptas quos itaque ipsam in voluptatem est. Iste eos blanditiis repudiandae. Earum deserunt enim molestiae ipsum perferendis recusandae saepe corrupti.",
+        author: {
+          name: "Tom Cook",
+          handle: "tomcook",
+          imageUrl:
+            "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+      },
+      // More testimonials...
+    ],
+    [
+      {
+        body: "Molestias ea earum quos nostrum doloremque sed. Quaerat quasi aut velit incidunt excepturi rerum voluptatem minus harum.",
+        author: {
+          name: "Leonard Krasner",
+          handle: "leonardkrasner",
+          imageUrl:
+            "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+      },
+      // More testimonials...
+    ],
+  ],
+];
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const features = [
   {
@@ -157,6 +225,94 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+          <div className="md:ml-52 lg:ml-64">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="sm:mx-auto max-w-xl mx-8 text-center">
+                <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">
+                  Testimonials
+                </h2>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-50 sm:text-4xl">
+                  Here's what others are saying about Chem Tools
+                </p>
+              </div>
+              <div
+                className="lg:absolute hidden inset-x-0 -z-20 lg:flex justify-center overflow-hidden blur-3xl"
+                aria-hidden="true"
+              >
+                <div
+                  className="aspect-[1318/752] w-full md:w-[82.375rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-40 lg:opacity-25"
+                  style={{
+                    clipPath:
+                      "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
+                  }}
+                />
+              </div>
+              <div className="sm:mx-auto mx-8 mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-200 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
+                <figure className="col-span-2 hidden sm:block sm:rounded-2xl sm:bg-black border-white border-2 sm:shadow-lg sm:ring-1 sm:ring-gray-900/5 xl:col-start-2 xl:row-end-1">
+                  <blockquote className="p-12 text-xl font-semibold leading-8 tracking-tight text-gray-50">
+                    <p>{`“${featuredTestimonial.body}”`}</p>
+                  </blockquote>
+                  <figcaption className="flex items-center gap-x-4 border-t border-gray-900/10 px-6 py-4">
+                    <img
+                      className="h-10 w-10 flex-none rounded-full bg-black"
+                      src={featuredTestimonial.author.imageUrl}
+                      alt=""
+                    />
+                    <div className="flex-auto">
+                      <div className="font-semibold">
+                        {featuredTestimonial.author.name}
+                      </div>
+                      <div className="text-gray-300">{`@${featuredTestimonial.author.handle}`}</div>
+                    </div>
+                  </figcaption>
+                </figure>
+                {testimonials.map((columnGroup, columnGroupIdx) => (
+                  <div
+                    key={columnGroupIdx}
+                    className="space-y-8 xl:contents xl:space-y-0"
+                  >
+                    {columnGroup.map((column, columnIdx) => (
+                      <div
+                        key={columnIdx}
+                        className={classNames(
+                          (columnGroupIdx === 0 && columnIdx === 0) ||
+                            (columnGroupIdx === testimonials.length - 1 &&
+                              columnIdx === columnGroup.length - 1)
+                            ? "xl:row-span-2"
+                            : "xl:row-start-1",
+                          "space-y-8"
+                        )}
+                      >
+                        {column.map((testimonial) => (
+                          <figure
+                            key={testimonial.author.handle}
+                            className="rounded-2xl bg-black border-white border-2 p-6 shadow-lg ring-1 ring-gray-900/5"
+                          >
+                            <blockquote className="text-gray-50">
+                              <p>{`“${testimonial.body}”`}</p>
+                            </blockquote>
+                            <figcaption className="mt-6 flex items-center gap-x-4">
+                              <img
+                                className="h-10 w-10 rounded-full bg-gray-50"
+                                src={testimonial.author.imageUrl}
+                                alt=""
+                              />
+                              <div>
+                                <div className="font-semibold">
+                                  {testimonial.author.name}
+                                </div>
+                                <div className="text-gray-300">{`@${testimonial.author.handle}`}</div>
+                              </div>
+                            </figcaption>
+                          </figure>
+                        ))}
+                      </div>
+                    ))}
+                  </div>
+                ))}
               </div>
             </div>
           </div>

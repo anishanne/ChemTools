52e8124fad1701481fef53d20fe756b28b1844f2
Hiptostee <joey.marra2007@gmail.com>
2023-09-17 22:23:25 -0400

    better 404 page

--- pages/404.js
+++ pages/404.js
@@ -4,6 +4,18 @@ export default function Example() {
   return (
     <>
       <main className="grid min-h-full place-items-center lg:bg-none bg-gradient-to-t from-black via-20% via-[#120126] to-black h-screen px-6 py-24 sm:py-32 lg:px-8">
+        <div
+          className="lg:absolute hidden inset-x-0 -z-20 lg:flex justify-center overflow-hidden blur-3xl"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[1318/752] w-full md:w-[82.375rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-40 lg:opacity-25"
+            style={{
+              clipPath:
+                "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
+            }}
+          />
+        </div>
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600">404</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">
@@ -12,19 +24,21 @@ export default function Example() {
           <p className="mt-6 text-base leading-7 text-gray-200">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
+
+          <img className="rounded-full m-8 mx-auto" src="/smurfcat.jpeg"></img>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
+            <Link
+              href="/"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Go back home
-            </a>
-            <a
+            </Link>
+            <Link
               href="mailto:mishrarnav@gmail.com?subject=Chem Tools Support"
               className="text-sm font-semibold cursor-pointer text-gray-200"
             >
               Contact support <span aria-hidden="true">&rarr;</span>
-            </a>
+            </Link>
           </div>
         </div>
       </main>

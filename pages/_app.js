d65a484beb6d232662ee5df11045a2affa97d7dc
Hiptostee <joey.marra2007@gmail.com>
2023-09-19 17:12:40 -0400

    analytics test

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
-
+import "@/styles/globals.css";
+import Script from "next/script";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-MGWE70ZG79"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {` 
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-MGWE70ZG79');
+  `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
